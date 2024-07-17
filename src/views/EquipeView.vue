3c6b5993ed0b5b4940281192c892bae970c83f0d
GabrielNebraska <gabrielviniciuscorrea204@gmail.com>
2024-07-17 10:53:02 -0300

    ProjectPi-router

---
+++ src/views/EquipeView.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+    name: 'EquipeView',
+    data() {
+        return {
+            equipe: [
+                {
+                    nome: 'Fulano',
+                    cargo: 'Desenvolvedor'
+                },
+                {
+                    nome: 'Ciclano',
+                    cargo: 'Designer'
+                },
+                {
+                    nome: 'Beltrano',
+                    cargo: 'Gerente de Projetos'
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<template>
+    <div>
+        <h1>Equipe</h1>
+        <ul>
+            <li v-for="membro in equipe" :key="membro.nome">
+                <h2>{{ membro.nome }}</h2>
+                <p>{{ membro.cargo }}</p>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+/* Your component-specific styles go here */
+</style>
