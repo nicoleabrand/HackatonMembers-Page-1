153dcaee3f07ad2a2891f4ea6dba1a65e04f9504
melloyasminn <melloyasminm@gmail.com>
2024-07-24 10:48:40 -0300

    .

--- src/App.vue
+++ src/App.vue
@@ -1,83 +1,38 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-
-    <div class="wrapper">
-      <h1>Membros Hackaton</h1>
-
+  <div class="wrapper">
+    <header>
+      <h1>Membros Hackathon</h1>
       <nav>
-        <RouterLink to="/Equipe">Equipe</RouterLink>
+        <RouterLink to="/Equipe" class="button">Equipe</RouterLink>
       </nav>
-    </div>
-  </header>
-
-  <RouterView />
+    </header>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+h1 {
+  font-family: 'Bebas Neue', sans-serif;
+  font-weight: 400;
+  font-style: normal;
+  font-size: 400%;
+  text-align: left;
+  padding: 2%;
+  padding-bottom: 0px;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.wrapper {
+  display: flex;
+  flex-direction:column;
+  background-color: cornsilk;
+  align-content: center;
 }
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+nav .button{
+  background-color: aliceblue;
+  margin: 3%;
+  margin-top: 0%;
+  text-decoration: none;
 }
 </style>
